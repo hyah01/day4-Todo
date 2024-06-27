597ea1b0d901af705cec72c25d19a8b78266e287
Hy <hyhuynh222@gmail.com>
2024-06-27 13:37:57 -0700

    Updated Service to get correct data
    Updated task complete functionality

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TodoService } from '../todo.service';
 
 @Component({
@@ -6,23 +6,44 @@ import { TodoService } from '../todo.service';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit{
+  // bounded these to input and textarea
   task: string = '';
   detail: string = '';
-  
-  constructor(private todoService: TodoService){
 
+  // inject service into class
+  constructor(private todoService: TodoService){
+  }
+
+  // load local data when refreshed
+  ngOnInit(){
+    this.todoService.loadData();
   }
 
   saveData() {
     this.todoService.saveData(this.task, this.detail);
+    console.log('Added: ', this.task)
+    this.task = ''; // Clear task when save
+    this.detail = ''; // Clear detail when save
   }
 
   get tasks() {
     return this.todoService.tasks;
   }
 
+  // delete data based on the key from localStorage
   deleteData(date: string) {
     this.todoService.deleteData(date);
+    console.log('Deleted: ', date)
+  }
+
+  toggleComplete(date: string){
+    let taskString = localStorage.getItem(date);
+    if (taskString){
+      let task = JSON.parse(taskString);
+      task.completed = !task.completed;
+      localStorage.setItem(date, JSON.stringify(task))
+      this.todoService.loadData();
+    }
   }
 }
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,31 +2,36 @@ export class TodoService {
   session: string[] = [];
   tasks: any[] = [];
 
-  ngOnInit(){
-    this.loadData();
-  }
-
   saveData(task: string, detail: string) {
-    if ((task != '')){
-      let date: string = (new Date()).toISOString();
-      let data = {task: task,detail: detail, date: date};
-      localStorage.setItem(date,JSON.stringify(data));
+    if (task != '') {
+      // make sure task is not empty but detail can be empty
+      let date: string = new Date().toISOString();
+      let data = { task: task, detail: detail, date: date, completed: false}; // store date so delete can use it
+      localStorage.setItem(date, JSON.stringify(data)); // set key to be the date, better usage for deletion and creation
       this.session.push(date);
-      this.loadData();
+      this.loadData(); // update website
     }
   }
 
   loadData() {
-    this.tasks = [];
-    this.session.forEach((taskKey) => {
-      const task = localStorage.getItem(taskKey);
-      if (task){
-        this.tasks.push(JSON.parse(task));
-      }
-    })
+    if (typeof localStorage != 'undefined') {
+      this.tasks = [];
+      Object.keys(localStorage).forEach((taskKey) => { // object.keys give all the key,value pair 
+        if (taskKey != 'debug') { // take care of debug so it doesn't try to parse it
+          const task = localStorage.getItem(taskKey);
+          if (task) {
+            try {
+              this.tasks.push(JSON.parse(task));
+            } catch (e) {
+              console.log('Invalid JSON in LocalStorage', task);
+            }
+          }
+        }
+      });
+    }
   }
 
-  deleteData(date:string){
+  deleteData(date: string) {
     localStorage.removeItem(date);
     this.loadData();
   }
